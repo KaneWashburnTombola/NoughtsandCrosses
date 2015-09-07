e975b901cfe45c7150e83509ae9d3276186adfb6
Kane Washburn <Kane.Washburn@tombola.com>
2015-09-07 15:42:00 +0100

    streamlined grunt copy task

---
+++ .grunt/copy-task.js
@@ -0,0 +1,32 @@
+/**
+ * Created by kane.washburn on 07/09/15.
+ */
+(function(){
+    'use strict';
+    module.exports={
+        mainappimages:{
+            cwd:'',
+            src:['main-app/app/images/**/*.*'],
+            dest:'.build/',
+            expand:true
+        },
+        mainappsounds:{
+            cwd:'',
+            src:['main-app/app/sounds/**/*.*'],
+            dest:'.build/',
+            expand:true
+        },
+        html:{
+            cwd:'',
+            src:['main-app/app/index.html'],
+            dest:'.build/',
+            expand:true
+        },
+        bower:{
+            cwd:'',
+            src:['bower_components/**/*.*'],
+            dest:'.build/main-app/app/thirdparty/',
+            expand:true
+        }
+    };
+})();
--- gruntfile.js
+++ gruntfile.js
@@ -3,38 +3,13 @@
  */
 (function(){
     'use strict';
+    var copyTask = require('./.grunt/copy-task');
     module.exports = function(grunt) {
         grunt.initConfig({
             pkg:grunt.file.readJSON('package.json'),
-            copy:{
-                mainappimages:{
-                    cwd:'',
-                    src:['main-app/app/images/batman.jpg'],
-                    dest:'.build/',
-                    expand:true
-                },
-                mainappsounds:{
-                    cwd:'',
-                    src:['main-app/app/sounds/ready aim fire.mp3'],
-                    dest:'.build/',
-                    expand:true
-                },
-                html:{
-                    cwd:'',
-                    src:['main-app/app/index.html'],
-                    dest:'.build/',
-                    expand:true
-                },
-                bower:{
-                    cwd:'',
-                    src:['bower_components/**/*.*'],
-                    dest:'.build/main-app/app/thirdparty/',
-                    expand:true
-                }
-            }
+            copy: copyTask
         });
         grunt.loadNpmTasks('grunt-contrib-copy');
         grunt.registerTask('default',['copy']);
     };
-
 })();
