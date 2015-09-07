3f2f764a6057ce40f2250b84e4b1dc927c26478f
Kane Washburn <Kane.Washburn@tombola.com>
2015-09-07 14:06:26 +0100

    created grunt copy task

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,22 @@
 /**
  * Created by kane.washburn on 07/09/15.
  */
+(function(){
+    'use strict';
+    module.exports = function(grunt) {
+        grunt.initConfig({
+            pkg:grunt.file.readJSON('package.json'),
+            copy:{
+                mainappimages:{
+                    cwd:'',
+                    src:['main-app/app/images/batman.jpg'],
+                    dest:'.build/',
+                    expand:true
+                }
+            }
+        });
+        grunt.loadNpmTasks('grunt-contrib-copy');
+        grunt.registerTask('default',['copy']);
+    };
+
+})();
