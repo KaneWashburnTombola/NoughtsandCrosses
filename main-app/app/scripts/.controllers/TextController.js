5a8c06e44f955d33a23484cc24ea604850503e96
Kane Washburn <Kane.Washburn@tombola.com>
2015-09-16 17:13:55 +0100

    all working! time to refactor

--- main-app/app/scripts/.controllers/TextController.js
+++ main-app/app/scripts/.controllers/TextController.js
@@ -3,44 +3,46 @@
     angular.module('Tombola.setup')
         .controller('TextContoller',['$scope','HttpCall',function($scope,httpCall){
             $scope.titleMessage = "Welcome to Noughts and Crosses";
-            $scope.playerOne = "";
-            $scope.playerTwo = "";
+            $scope.playerOne = "human";
+            $scope.playerTwo = "human";
             $scope.gameReady=false;
-            var currentPlayer;
+            var currentPlayer='1';
             $scope.startGame=function(){
-                httpCall.newGame($scope.playerOne,$scope.playerTwo,$scope.gameBoard).then(
+                httpCall.newGame($scope.playerOne,$scope.playerTwo).then(
                     function(response){
                         $scope.gameReady=true;
                         $scope.gameBoard=response.data.gameboard;
-                        currentPlayer='1';
                     },
                     function(){
                     });
             };
             $scope.makeTurn=function(number){
+
+                if($scope.playerOne === 'human' && $scope.playerTwo !== 'human'){
+                    currentPlayer='1';
+                }
+                else if($scope.playerOne !== 'human' && $scope.playerTwo === 'human'){
+                        currentPlayer='2';
+                    }
                 if($scope.gameBoard[number]!=='0'){
                     return;
                 }
-                httpCall.newTurn(currentPlayer,number,$scope.gameBoard).then(
+                httpCall.newTurn(currentPlayer,number).then(
                     function(data){
                         $scope.gameBoard=data.gameboard;
-                        $scope.gameBoard=$scope.gameBoard.substr(0,number) + currentPlayer + $scope.gameBoard.substr(number+1);
+                        console.log(currentPlayer);
                         if(data.outcome==='Win'){
                           //TODO win animation thingy
                         }
+                        if($scope.playerOne === 'human' && $scope.playerTwo === 'human') {
+                            currentPlayer = currentPlayer === '1' ? '2' : '1';
+                        }
                     },
-                    function(){
+                    function(data){
+                        console.log("makeMove failed"+data);
                     });
-                if($scope.playerOne==='human'&& $scope.playerTwo==='human'){
-                    currentPlayer=currentPlayer==='1'? '2':'1';
-                }
-                else if($scope.playerOne==='human'&& $scope.playerTwo!=='human'){
-                    currentPlayer='1';
-                }
-                else if($scope.playerOne!=='human'&& $scope.playerTwo==='human'){
-                        console.log("currentplayer changing to 2");
-                    currentPlayer='2';
-                }
             };
+
+
         }]);
 })();
