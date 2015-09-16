1546b09609530ec36b5ce25aa22f481074657134
Kane Washburn <Kane.Washburn@tombola.com>
2015-09-16 14:09:08 +0100

    fixing api interaction

--- main-app/app/scripts/.controllers/TextController.js
+++ main-app/app/scripts/.controllers/TextController.js
@@ -5,26 +5,41 @@
             $scope.titleMessage = "Welcome to Noughts and Crosses";
             $scope.playerOne = "";
             $scope.playerTwo = "";
-            $scope.gameBoard='000000000';
-            var currentPlayer = '1';
+            $scope.gameReady=false;
+            var currentPlayer;
             $scope.startGame=function(){
-                httpCall.newGame($scope.playerOne,$scope.playerTwo,$scope.gameBoard);
+                httpCall.newGame($scope.playerOne,$scope.playerTwo,$scope.gameBoard).then(
+                    function(response){
+                        $scope.gameReady=true;
+                        $scope.gameBoard=response.data.gameboard;
+                        currentPlayer='1';
+                    },
+                    function(){
+                    });
             };
             $scope.makeTurn=function(number){
                 if($scope.gameBoard[number]!=='0'){
                     return;
                 }
-                httpCall.newTurn(currentPlayer,number,$scope.gameBoard);
-                $scope.gameBoard=$scope.gameBoard.substr(0,number) + currentPlayer + $scope.gameBoard.substr(number+1);
-                currentPlayer=currentPlayer==='1'? '2':'1';
-            };
-            var winControl=function(response){
-                console.log("collecting response"+response);
-                if(response.data.winner==="1"){
-                    alert("Player 1 wins!");
+                httpCall.newTurn(currentPlayer,number,$scope.gameBoard).then(
+                    function(data){
+                        $scope.gameBoard=data.gameboard;
+                        $scope.gameBoard=$scope.gameBoard.substr(0,number) + currentPlayer + $scope.gameBoard.substr(number+1);
+                        if(data.outcome==='Win'){
+                          //TODO win animation thingy
+                        }
+                    },
+                    function(){
+                    });
+                if($scope.playerOne==='human'&& $scope.playerTwo==='human'){
+                    currentPlayer=currentPlayer==='1'? '2':'1';
                 }
-                else{
-                    alert("Player 2 wins!");
+                else if($scope.playerOne==='human'&& $scope.playerTwo!=='human'){
+                    currentPlayer='1';
+                }
+                else if($scope.playerOne!=='human'&& $scope.playerTwo==='human'){
+                        console.log("currentplayer changing to 2");
+                    currentPlayer='2';
                 }
             };
         }]);
--- main-app/app/scripts/.services/http-service.js
+++ main-app/app/scripts/.services/http-service.js
@@ -1,24 +1,29 @@
 (function () {
     'use strict';
    angular.module('Tombola.setup')
-       .service('HttpCall',['$http',function($http) {
+       .service('HttpCall',['$http','$q',function($http,$q) {
            var me = this;
            me.newGame=function(player1Type, player2Type){
+               var defered= $q.defer();
                $http.post('http://eutaveg-01.tombola.emea:35000/api/v1.0/newgame', {"player1": player1Type, "player2": player2Type},{"withCredentials":"true"})
                    .then(function (response) {
+                       defered.resolve(response);
                    },
                    function (response) {
+                       defered.reject(response.data);
                    });
+               return defered.promise;
            };
            me.newTurn=function(playerNumber,chosenSquare){
+               var defered= $q.defer();
                $http.post('http://eutaveg-01.tombola.emea:35000/api/v1.0/makemove',{"playerNumber":playerNumber,"chosenSquare":chosenSquare},{"withCredentials":"true"})
                    .then(function(response){
-                       console.log("returning the response"+response);
-
+                       defered.resolve(response.data);
                    },
                    function(response){
-
+                       defered.reject(response.data);
                    });
+               return defered.promise;
            };
        }]);
 })();
