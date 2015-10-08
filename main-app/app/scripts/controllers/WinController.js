d1b93b9d5764eabf31bab56124d78ac0bd4b0c64
Kane Washburn <Kane.Washburn@tombola.com>
2015-10-08 09:41:40 +0100

    getting rid of console logs

--- main-app/app/scripts/controllers/WinController.js
+++ main-app/app/scripts/controllers/WinController.js
@@ -2,15 +2,11 @@
     'use strict';
     angular.module('Tombola.Win')
         .controller('WinController',['$scope','$state','WinDecider',function($scope,$state,winDecider){
-
             $scope.showWinner=function(){
-                console.log(winDecider.winner);
                 if(winDecider.winner===1){
-                    console.log(winDecider.winner);
                     $state.go('player1Win');
                 }
                 else{
-                    console.log(winDecider.winner);
                     $state.go('player2Win');
                 }
             };
