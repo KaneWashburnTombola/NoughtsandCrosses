a4e4c703868b01673179bb1396f3eb42e6e377ce
Kane Washburn <Kane.Washburn@tombola.com>
2015-10-08 13:46:01 +0100

    changed name of service file to ApiProxy

--- .grunt/server-task.js
+++ .grunt/server-task.js
@@ -1,11 +1,7 @@
 (function () {
     'use strict';
         var express = require('express');
-       // var morgan = require('morgan');
-
         var app = express();
-
-        //app.use(morgan('combined'));
         app.use(express.static(process.cwd() + '/.build/main-app/app', {maxAge:86400000}));
         module.exports = app;
 })();
