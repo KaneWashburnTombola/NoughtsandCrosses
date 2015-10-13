0a19770df93fd6f9dec813223d337da611e3637c
Kane Washburn <Kane.Washburn@tombola.com>
2015-10-13 10:59:43 +0100

    added first api call test

--- main-app/app/testing/ApiProxy-Test.js
+++ main-app/app/testing/ApiProxy-Test.js
@@ -7,8 +7,10 @@ describe('Api Proxy Calls',function(){
            $http = $injector.get('$httpBackend');
            apiProxy=$injector.get('ApiProxy');
            startCall =$http.when('POST','http://eutaveg-01.tombola.emea:35000/api/v1.0/newGame')
-               .respond({})
+               .respond({'outcome':'Continue','gameboard':'000000000','winner':0});
        });
    });
-
+    it('should make call to api',function(){
+        $http.expectPOST('http://eutaveg-01.tombola.emea:35000/api/v1.0/newGame',{"player1": player1Type,"player2": player2Type},{"withCredentials":"true"});
+    });
 });
