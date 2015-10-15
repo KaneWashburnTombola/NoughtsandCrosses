e15c517c821b22f508043a48bd4b7ff52b13ac3a
Kane Washburn <Kane.Washburn@tombola.com>
2015-10-15 12:02:55 +0100

    started testing

--- main-app/app/testing/directives/square-directive.test.js
+++ main-app/app/testing/directives/square-directive.test.js
@@ -2,13 +2,13 @@ describe('Unit testing squares',function(){
     var $compile,
         $rootScope;
     beforeEach(module('Tombola.SquareDirective'));
-    beforeEach(inject(function(_$compile_, _$rootScope_){
-        $compile = _$compile_;
-        $rootScope = _$rootScope_;
+    beforeEach(inject(function($compile, $rootScope){
+        compile = $compile;
+        scope = $rootScope.$new();
     }));
     it('Replaces the element with the appropriate content', function() {
         var element = $compile('<square></square>')($rootScope);
+        expect(element.html()).toContain('ng-click="makeTurn('+element.squareNumber+')" class= "cell player{{gameBoard['+element.squareNumber+']}}"');
         $rootScope.$digest();
-        expect(element.html()).toContain('ng-click="makeTurn('+element.squareNumber+')" class= "cell player{{gameBoard['+element.squareNumber+']}}"')
     });
 });
