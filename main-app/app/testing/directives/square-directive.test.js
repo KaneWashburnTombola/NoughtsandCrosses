7b3f1d3be8f43f87fbf7a82cba6254594a16e604
Kane Washburn <Kane.Washburn@tombola.com>
2015-10-16 10:55:05 +0100

    continued to add to test ( get undefined not a function error )

--- main-app/app/testing/directives/square-directive.test.js
+++ main-app/app/testing/directives/square-directive.test.js
@@ -4,10 +4,11 @@ describe('Unit testing squares',function(){
     var element;
     var squareNumber = 4;
     var sandbox;
+    var ApiProxy;
       beforeEach(function(){
         module('Tombola.SquareDirective');
           module(function($provide){
-              $provide
+              $provide.service('ApiProxy',mocks.ApiProxy);
           });
         inject(function($injector){
             element = angular.element('<div ng-click="makeTurn('+squareNumber+')" class= "cell player{{gameBoard['+squareNumber+']}}"></div>');
@@ -16,10 +17,17 @@ describe('Unit testing squares',function(){
         });
           compile(element)(scope);
           scope.$digest();
+          sandbox = sinon.sandbox.create();
+          ApiProxy = sinon.mock(mocks.ApiProxy);
     });
 
     it('checks it sets up element', function() {
         element.attr('ng-click').should.equal('makeTurn(4)');
-        //element.attr('class').should.equal('cell player');
+        ApiProxy.newTurn('1','4');
+        element.attr('class').should.equal('cell player1');
+    });
+    afterEach(function(){
+       ApiProxy.verify();
+        sandbox.restore();
     });
 });
