33b54f9c6be8dea03c374f471c469493d9d71db4
Kane Washburn <Kane.Washburn@tombola.com>
2015-10-16 11:03:08 +0100

    added API mock

--- main-app/app/testing/controllers/text-controller.test.js
+++ main-app/app/testing/controllers/text-controller.test.js
@@ -1,14 +1,24 @@
 describe('TextController',function(){
+    var sandbox;
+    var ApiProxy;
+    var PlayerSwitcher;
     beforeEach(function() {
         module('Tombola.Setup');
+        module(function($provide){
+            $provide.service('ApiProxy',mocks.ApiProxy);
+        });
         inject(function(_$controller_){
         controller= _$controller_;
         });
+        sandbox = sinon.sandbox.create();
+        ApiProxy = sinon.mock(mocks.ApiProxy);
     });
-    it('see if values are initialised',function(){
-        var $scope={};
-        var $state={};
-        var textController = controller('TextController',{$scope:$scope,$state:$state});
+    afterEach(function() {
+        ApiProxy.verify();
+        sandbox.restore();
+    });
+        it('see if values are initialised',function(){
+        var textController = controller('TextController');
         $scope.titleMessage.should.equal('Welcome to Noughts and Crosses');
     });
 });
