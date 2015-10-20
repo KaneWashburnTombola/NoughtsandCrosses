2868b14cdadaba29a3da4d6df2eb99fa69ad5d51
Kane Washburn <Kane.Washburn@tombola.com>
2015-10-20 13:44:48 +0100

    fixed directive test as well as general cleanup

--- main-app/app/testing/directives/square-directive.test.js
+++ main-app/app/testing/directives/square-directive.test.js
@@ -3,30 +3,19 @@ describe('Unit testing squares',function(){
     var scope;
     var element;
     var squareNumber = 4;
-    var sandbox;
-    var ApiProxySpy;
       beforeEach(function(){
         module('Tombola.SquareDirective');
-          module(function($provide){
-              $provide.service('ApiProxy',mocks.ApiProxy);
-          });
         inject(function($compile,$rootScope){
             element = angular.element('<div ng-click="makeTurn('+squareNumber+')" class= "cell player{{gameBoard['+squareNumber+']}}"></div>');
             compile = $compile;
             scope = $rootScope.$new();
         });
+          scope.gameBoard='000010000';
           compile(element)(scope);
           scope.$digest();
-          sandbox = sinon.sandbox.create();
-          ApiProxySpy = sinon.sandbox.spy(mocks.ApiProxy,'newTurn');
     });
     it('checks it sets up element', function() {
         element.attr('ng-click').should.equal('makeTurn(4)');
-        ApiProxySpy.newTurn('1','4');
         element.attr('class').should.equal('cell player1');
-    });
-    afterEach(function(){
-       ApiProxySpy.verify();
-        sandbox.restore();
     });
 });
