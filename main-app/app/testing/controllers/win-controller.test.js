7bf69249e14e37a3c0244d0e8eec6fc64b7163cf
Kane Washburn <Kane.Washburn@tombola.com>
2015-10-21 10:08:55 +0100

    provided mock and set up variables

--- main-app/app/testing/controllers/win-controller.test.js
+++ main-app/app/testing/controllers/win-controller.test.js
@@ -1,8 +1,14 @@
 describe('Testing if the win controller sends player to correct state',function(){
+    var scope;
+    var controller;
+    var winDecider;
     beforeEach(function(){
        module('Tombola.Win');
         module(function($provide){
             $provide.service('WinDecider',mocks.WinDecider);
+        });
+        inject(function(){
+
         });
     });
 });
