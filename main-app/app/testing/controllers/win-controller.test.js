61dba4e3cdbb3ca717cf25fbd5e2777e2058a9e2
Kane Washburn <Kane.Washburn@tombola.com>
2015-10-21 10:57:39 +0100

    added it statement

--- main-app/app/testing/controllers/win-controller.test.js
+++ main-app/app/testing/controllers/win-controller.test.js
@@ -1,14 +1,27 @@
 describe('Testing if the win controller sends player to correct state',function(){
     var scope;
+    var sandbox;
     var controller;
-    var winDecider;
+    var WinDecider;
     beforeEach(function(){
        module('Tombola.Win');
         module(function($provide){
             $provide.service('WinDecider',mocks.WinDecider);
         });
-        inject(function(){
-
+        inject(function($rootScope,$controller){
+            scope=$rootScope.$new();
+            controller= $controller('WinController',{
+                $scope:scope,
+                $state:mocks.$state,
+                WinDecider:mocks.WinDecider
+            });
         });
+        sandbox=sinon.sandbox.create();
+    });
+    it('sends player to appropriate state',function(){
+        //WinDecider.winner=1;
+        var stateStub=sinon.sandbox.stub(mocks.$state,'go');
+        scope.showWinner();
+
     });
 });
