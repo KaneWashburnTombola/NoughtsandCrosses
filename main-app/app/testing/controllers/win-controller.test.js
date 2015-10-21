bc95931dab03c994ac2835b7e31bb32f9e0b564a
Kane Washburn <Kane.Washburn@tombola.com>
2015-10-21 10:01:13 +0100

    added win decider mock

--- main-app/app/testing/controllers/win-controller.test.js
+++ main-app/app/testing/controllers/win-controller.test.js
@@ -1,6 +1,8 @@
 describe('Testing if the win controller sends player to correct state',function(){
     beforeEach(function(){
        module('Tombola.Win');
-        
+        module(function($provide){
+            $provide.service('WinDecider',mocks.WinDecider);
+        });
     });
 });
