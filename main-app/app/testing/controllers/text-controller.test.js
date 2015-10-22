2f8a4022d45bff7a95e5015212174c2669500062
Kane Washburn <Kane.Washburn@tombola.com>
2015-10-22 10:56:15 +0100

    done text controller test refactored win controller test

--- main-app/app/testing/controllers/text-controller.test.js
+++ main-app/app/testing/controllers/text-controller.test.js
@@ -1,47 +1,52 @@
-describe('TextController',function(){
-    var sandbox;
-    var ApiProxy;
-    var PlayerSwitcher;
-    var controller;
-    var scope;
-    var q;
-    var deferred;
-    beforeEach(function() {
-        module('Tombola.Setup');
-        module(function($provide){
-            $provide.service('ApiProxy',mocks.ApiProxy);
-            $provide.service('PlayerSwitcher',mocks.PlayerSwitcher);
-        });
-        inject(function($rootScope,$controller,$q){
-            scope=$rootScope.$new();
-            q=$q;
-            deferred=$q.defer();
-            controller= $controller('TextController',{
-                $scope:scope,
-                $state:mocks.$state,
-                ApiProxy:mocks.ApiProxy,
-                PlayerSwitcher:mocks.PlayerSwitcher
+(function () {
+    'use strict';
+    describe('TextController',function(){
+        var sandbox,
+            controller,
+            scope,
+            q,
+            deferred,
+            stateSpy,
+            playerSwitchSpy,
+            apiProxyStub;
+        beforeEach(function(){
+            module('Tombola.Setup');
+            inject(function($rootScope,$controller,$q){
+                scope=$rootScope.$new();
+                q=$q;
+                deferred=$q.defer();
+                controller= $controller('TextController',{
+                    $scope:scope,
+                    $state:mocks.$state,
+                    ApiProxy:mocks.ApiProxy,
+                    PlayerSwitcher:mocks.PlayerSwitcher
+                });
             });
+            sandbox = sinon.sandbox.create();
+            playerSwitchSpy=sinon.sandbox.spy(mocks.PlayerSwitcher,'playerSwitch');
+            stateSpy=sinon.sandbox.spy(mocks.$state,'go');
         });
-        sandbox = sinon.sandbox.create();
-    });
-    afterEach(function() {
-    });
+        afterEach(function() {
+            sandbox.restore();
+        });
         it('see if values are initialised',function(){
             scope.titleMessage.should.equal('Welcome to Noughts and Crosses');
             scope.playerOne.should.equal('human');
             scope.playerTwo.should.equal('human');
         });
-    it('should make newGame call to API and then initialise game state',function(){
-        ApiProxy=sinon.sandbox.stub(mocks.ApiProxy,'newGame',function(){
-            return deferred.promise;
+        it('should make newGame call to API and then initialise game state',function(){
+            apiProxyStub=sinon.sandbox.stub(mocks.ApiProxy,'newGame',function(){
+                return deferred.promise;
+            });
+            scope.startGame();
+            deferred.resolve({data:{gameboard:'000000000'}});
+            scope.$digest();
+            playerSwitchSpy.should.have.been.calledOnce.calledWithExactly(scope.playerOne,scope.playerTwo,'000000000');
+            stateSpy.should.have.been.calledOnce.calledWithExactly('playing');
         });
-        PlayerSwitcher=sandbox.spy(mocks.PlayerSwitcher,'playerSwitch');
-        $state=sandbox.spy(mocks.$state,'go');
-        scope.startGame();
-        deferred.resolve({data:{gameboard:'000000000'}});
-        scope.$digest();
-        PlayerSwitcher.should.have.been.calledOnce.calledWithExactly(scope.playerOne,scope.playerTwo,'000000000');
-
+        it('should return to lobby state',function(){
+            scope.lobbyReturn();
+           stateSpy.should.have.been.calledOnce.calledWithExactly('lobby');
+        });
     });
-});
+})();
